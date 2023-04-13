68fc3c215444be131705d9be43348cc0da15a670
stivowaGod <129147053+stivowaGod@users.noreply.github.com>
2023-04-13 20:52:56 +0300

    Add files via upload

---
+++ one.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    echo 'Remember PHP. </br> ';
+	print  "TYG All the glory be to God for His faithfulness</br>
+	Now till forever</br> </br>";
+	echo "Things you need to do networking</br>
+	1 Network switch</br>
+	2 untwisted pair cat 6 </br>
+	3 Rj45</br>
+	4 Faceplate</br>
+	5 parch panel</br>
+	6 Cabinet box</br>
+	"
+?>
+	 
+</body>
+
+</html>
